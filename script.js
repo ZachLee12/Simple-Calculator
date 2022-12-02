b0ed46e623cc3adb96a618ab4187aa2fbcdf98d5
ZachLee12 <leezhengyang22@gmail.com>
2022-12-02 22:10:37 +0000

    Fix num1 decimal point bug

--- script.js
+++ script.js
@@ -42,15 +42,19 @@ let display = document.querySelector(".display")
 let num1Display = document.querySelector(".num1")
 let num2Display = document.querySelector(".num2")
 let operatorDisplay = document.querySelector(".operatorDisplay")
-let decimalPointNum1 = false;
+let decimalPointNum1 = true;
 let decimalPointNum2 = false;
 
 operators.forEach((button) => {
     button.addEventListener("click", (e) => {
         if (num1 === "") return -1;
         firstOperatorClicked = true;
+        decimalPointNum1 = false;
 
         if (num2 != "") {
+            if (num2 === ".") {
+                num2 = 0;
+            }
             result = operate(operator, parseFloat(num1), parseFloat(num2))
 
             console.log("result: " + result);
@@ -79,6 +83,9 @@ numberButtons.forEach((button) => {
 
 document.querySelector(".equals").addEventListener("click", (e) => {
     if (num2 != "") {
+        if (num2 === ".") {
+            num2 = 0;
+        }
         result = operate(operator, parseFloat(num1), parseFloat(num2))
         console.log("result: " + result);
         num1 = Math.round(result * 100) / 100;
@@ -91,17 +98,7 @@ document.querySelector(".equals").addEventListener("click", (e) => {
 })
 
 document.querySelector(".clear").addEventListener("click", (e) => {
-    display.textContent = "display"
-    num1Display.textContent = ""
-    num2Display.textContent = ""
-    operatorDisplay.textContent = ""
-    num1 = "";
-    num2 = "";
-    result = 0;
-    operator = "";
-    firstOperatorClicked = false;
-
-    console.log("Clicked: " + e.target.textContent)
+    clear();
 })
 
 document.getElementById("%").addEventListener("click", () => {
@@ -111,7 +108,7 @@ document.getElementById("%").addEventListener("click", () => {
 })
 
 document.querySelector(".decimalPoint").addEventListener("click", (e) => {
-    if (!decimalPointNum1) {
+    if (decimalPointNum1 == true) {
         num1 += e.target.textContent;
     } else {
         if (firstOperatorClicked) {
@@ -123,15 +120,10 @@ document.querySelector(".decimalPoint").addEventListener("click", (e) => {
 
     }
     updateDisplay()
-    decimalPointNum1 = true;
-})
-
-function updateDisplay() {
-    display.textContent = Math.round(result * 100) / 100;
-    num1Display.textContent = num1;
-    num2Display.textContent = num2;
-    operatorDisplay.textContent = operator
+    decimalPointNum1 = false;
+    
 }
+)
 
 document.querySelector(".delete").addEventListener("click", () => {
     if (!firstOperatorClicked) {
@@ -142,5 +134,23 @@ document.querySelector(".delete").addEventListener("click", () => {
     updateDisplay();
 })
 
-//BUG sequence: 9, +, =, 3, =
-//then it breaks
+function updateDisplay() {
+    display.textContent = Math.round(result * 100) / 100;
+    num1Display.textContent = num1;
+    num2Display.textContent = num2;
+    operatorDisplay.textContent = operator
+}
+
+function clear() {
+    display.textContent = "display"
+    num1Display.textContent = ""
+    num2Display.textContent = ""
+    operatorDisplay.textContent = ""
+    num1 = "";
+    num2 = "";
+    result = 0;
+    operator = "";
+    firstOperatorClicked = false;
+
+    console.log("Clicked: " + e.target.textContent)
+}
